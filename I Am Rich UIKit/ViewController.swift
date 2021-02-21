018aee4a3751461b4d306f3ff85791e392d289f0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-21 23:20:41 +0700

    initiate

---
+++ I Am Rich UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich UIKit
+//
+//  Created by Runyasak Chaengnaimuang on 21/2/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
